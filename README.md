a63d1b4ef725f00122b51f99f1de3913b9d3d0de
Davide Brognoli <davide.brognoli@codiceplastico.com>
2023-12-01 13:24:05 +0100

    Change project structure

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Advent of Code
 
 This project contains solutions for Advent of Code.
+
 ## Installation
 
 To install the project dependencies, run the following command:
@@ -12,11 +13,11 @@ npm install
 ## Commands
 
 - `npm test`: Runs all tests in the project.
-- `npm run test:day --day=1`: Runs the tests for a specific day. Replace `1` with the number of the day for which you want to run the tests.
 - `npm run generate:day --day=1`: Generates the files for a specific day. Replace `1` with the number of the day for which you want to generate the files.
 - `npm run run:day --day=1`: Runs the scripts for a specific day. Replace `1` with the number of the day for which you want to run the scripts.
 
 ## Project Structure
+
 ```
 advent-of-code/
 ├── package.json
@@ -24,15 +25,23 @@ advent-of-code/
 ├── node_modules/
 ├── day1/
 │   ├── part1.js
+│   ├── part1.test.js
 │   ├── part2.js
-│   ├── day.test.js
-│   └── input.txt
+│   ├── part2.test.js
+│   └── input1.txt
+│   └── input2.txt
+│   └── test1.txt
+│   └── test2.txt
 ├── day2/
 │   ├── part1.js
+│   ├── part1.test.js
 │   ├── part2.js
-│   ├── day.test.js
-│   └── input.txt
+│   ├── part2.test.js
+│   └── input1.txt
+│   └── input2.txt
+│   └── test1.txt
+│   └── test2.txt
 ...
 ```
 
-In this structure, `day1/`, `day2/`, etc. are directories for each day of Advent of Code, which contains two JavaScript files for the two parts of that day's problem, a test file, an `input.txt` file with the input data for the problem and a `test.txt` file with the test input data.
+In this structure, `day1/`, `day2/`, etc. are directories for each day of Advent of Code, which contains two JavaScript files for the two parts of that day's problem (`part1.js` and `part2.js`), corresponding test files (`part1.test.js` and `part2.test.js`), and input/test data files (`input1.txt`, `input2.txt`, `test1.txt`, and `test2.txt`).
--- generateDay.js
+++ generateDay.js
@@ -1,55 +1,58 @@
-const fs = require('fs');
-const path = require('path');
-
-// Prendi il giorno da creare dalla riga di comando
-const day = process.argv[2];
-
-// Template per i file script e di test
-const scriptTemplate = `
 const fs = require("fs");
 const path = require("path");
 
-function solve(inputFile = 'input.txt') {
+const day = process.argv[2];
+
+function writeSolutionScriptToFile(dayDir, fileName, part) {
+  const scriptTemplate = `const fs = require("fs");
+const path = require("path");
+
+function solve(inputFile = 'input${part}.txt') {
   const input = fs.readFileSync(path.join(__dirname, inputFile), 'utf-8');
   const lines = input.split("");
 
   const result = lines.length;
   return result;
 }
-
 module.exports = solve;
 `;
+  fs.writeFileSync(path.join(dayDir, fileName), scriptTemplate);
+}
 
-const testTemplate = `
-const solvePart1 = require("./part1");
-const solvePart2 = require("./part2");
+function writeTestScriptToFile(dayDir, fileName, part) {
+  const testTemplate = `const solvePart = require("./part${part}");
 
-test("solvePart1 solves the problem correctly", () => {
-  const result = solvePart1("test.txt");
+test("solvePart${part} solves the problem correctly with testdata", () => {
+  const result = solvePart("test1.txt");
   const expectedResult = 0;
 
   expect(result).toEqual(expectedResult);
 });
 
-test("solvePart2 solves the problem correctly", () => {
-  const result = solvePart2("test.txt");
+test("solvePart${part} solves the problem correctly", () => {
+  const result = solvePart();
   const expectedResult = 0;
 
   expect(result).toEqual(expectedResult);
 });
 `;
+  fs.writeFileSync(path.join(dayDir, fileName), testTemplate);
+}
 
 // Crea la cartella per il giorno
 const dayDir = path.join(__dirname, `day${day}`);
 if (!fs.existsSync(dayDir)) {
-    fs.mkdirSync(dayDir);
+  fs.mkdirSync(dayDir);
 }
 
 // Crea i file script e di test
-fs.writeFileSync(path.join(dayDir, 'part1.js'), scriptTemplate);
-fs.writeFileSync(path.join(dayDir, 'part2.js'), scriptTemplate);
-fs.writeFileSync(path.join(dayDir, 'day.test.js'), testTemplate);
+writeSolutionScriptToFile(dayDir, "part1.js", 1);
+writeSolutionScriptToFile(dayDir, "part2.js", 2);
+writeTestScriptToFile(dayDir, "part1.test.js", 1);
+writeTestScriptToFile(dayDir, "part2.test.js", 2);
 
 // Crea il file di input vuoto
-fs.writeFileSync(path.join(dayDir, 'input.txt'), '');
-fs.writeFileSync(path.join(dayDir, 'test.txt'), '');
+fs.writeFileSync(path.join(dayDir, "input1.txt"), "");
+fs.writeFileSync(path.join(dayDir, "input2.txt"), "");
+fs.writeFileSync(path.join(dayDir, "test1.txt"), "");
+fs.writeFileSync(path.join(dayDir, "test2.txt"), "");
